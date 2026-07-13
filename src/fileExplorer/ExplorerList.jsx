198649f6f61b39f1386704697add2260349798ee
Nickop21 <ny8244@gmail.com>
2026-07-13 23:08:13 +0530

    childern addind node bug fixed

--- src/fileExplorer/ExplorerList.jsx
+++ src/fileExplorer/ExplorerList.jsx
@@ -1,59 +1,72 @@
 import React, { useState } from "react";
 
-const ExplorerList = ({ list,setFileData }) => {
-    const [isExpandable,setIsExpandable]=useState(false)
-     const toggleFolder = (id) => {
+const ExplorerList = ({ list, setFileData }) => {
+  const [isExpandable, setIsExpandable] = useState(false);
+  const toggleFolder = (id) => {
     setIsExpandable((prev) => ({
       ...prev,
       [id]: !prev[id],
     }));
   };
 
-function addNodeTOList(parentId){
-  const nameadd=prompt("Enter Your Name")
+  function addNodeTOList(parentId) {
+    const nameadd = prompt("Enter Your Name");
 
-  const updateTree=(list)=>{
-    return list.map((node)=>{
-      if(node.id==parentId){
-        return{
-          ...node,children:[
-            ...node.children,
-            {
-             id:"1233" ,name:nameadd,isFolder:true,children:[]
-            }
-          ]
+    const updateTree = (list) => {
+      return list.map((node) => {
+        if (node.id == parentId) {
+          return {
+            ...node,
+            children: [
+              ...node.children,
+              {
+                id: nameadd,
+                name: nameadd,
+                isFolder: true,
+                children: [],
+              },
+            ],
+          };
         }
-      }
 
-      if (node.children) {
-        return{
-          ...node,children:updateTree(node.children)
+        if (node.children) {
+          return {
+            ...node,
+            children: updateTree(node.children),
+          };
         }
-        
-      }
 
-      return node
-
-
-    })
+        return node;
+      });
+    };
+    setFileData((prev) => updateTree(prev));
   }
-  setFileData((prev)=>updateTree(prev))
-
-}
 
   return (
     <>
       {list?.map((data) => (
-        <div className={` parent-list ${!data.isFolder && "ml-6"}`} key={data?.id}>
+        <div
+          className={` parent-list ${!data.isFolder && "ml-6"}`}
+          key={data?.id}
+        >
           <div>
-            {data.isFolder && <span className="mr-1 cursor-pointer" onClick={()=>toggleFolder(data.id)}>{isExpandable[data.id] ? "➡️" :"⬇️"}</span>
-}
-             <span className="mr-1">{data.isFolder? "📁" :"📃"}</span>
+            {data.isFolder && (
+              <span
+                className="mr-1 cursor-pointer"
+                onClick={() => toggleFolder(data.id)}
+              >
+                {isExpandable[data.id] ? "➡️" : "⬇️"}
+              </span>
+            )}
+            <span className="mr-1">{data.isFolder ? "📁" : "📃"}</span>
             <span>{data?.name}</span>
-             <span className="mr-2" onClick={()=>addNodeTOList(data.id)}>{data.isFolder && "  ➕" }</span>
-
+            <span className="mr-2" onClick={() => addNodeTOList(data.id)}>
+              {data.isFolder && "  ➕"}
+            </span>
           </div>
-          {isExpandable[data.id] && data?.children && <ExplorerList list={data.children} />}
+          {isExpandable[data.id] && data?.children && (
+            <ExplorerList list={data.children} setFileData={setFileData} />
+          )}
         </div>
       ))}
     </>
