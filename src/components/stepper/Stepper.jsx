6c53dee68c0848d09460fdd9b05e196c5b77fd75
Nickop21 <ny8244@gmail.com>
2026-07-15 22:38:21 +0530

    stepper functionality  + css changes

--- src/components/stepper/Stepper.jsx
+++ src/components/stepper/Stepper.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./stepper.css";
-import Steps from "./steps";
+import Steps from "./Steps";
 
 const Stepper = () => {
+  const [currIndex, setCurrIndex] = useState(0);
   const stepperData = [
     {
       name: "personal info",
@@ -25,11 +26,27 @@ const Stepper = () => {
       checked: false,
     },
   ];
+  function changeIndex(index) {
+    if (stepperData.length > index && index >= 0) {
+      setCurrIndex(index);
+    }
+  }
+
   return (
     <div className="container-step">
-      <Steps stepperData={stepperData} />
-      <button className="back button">Back</button>
-      <button className="next button">Continue</button>
+      <Steps stepperData={stepperData} currIndex={currIndex} />
+      <button
+        className="back button"
+        onClick={() => changeIndex(currIndex - 1)}
+      >
+        Back
+      </button>
+      <button
+        className="next button"
+        onClick={() => changeIndex(currIndex + 1)}
+      >
+        Continue
+      </button>
     </div>
   );
 };
--- src/components/stepper/Steps.jsx
+++ src/components/stepper/Steps.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const Steps = ({stepperData}) => {
+const Steps = ({ stepperData, currIndex }) => {
   return (
     <div className="stepper-container">
       {stepperData.map((data, index) => (
         <div className="steppers" key={index}>
-           {index!=0 && <div className="connecting-line"></div>}
-          <div className="stepper-box">{index + 1}</div>
+          {index != 0 && <div className={`connecting-line color-transition ${currIndex>=index && "green" }`}></div>}
+          <div
+            className={`stepper-box color-transition ${currIndex>=index && "green" }`} >
+            {index + 1}
+          </div>
           <span>{data.name}</span>
         </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default Steps
+export default Steps;
